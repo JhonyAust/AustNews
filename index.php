4abb04deb6a326da441ffe59031d28a6d8aca67c
Jhony <jhonyaustcse37@gmail.com>
2019-07-07 13:27:26 +0600

    commited

--- index.php
+++ index.php
@@ -273,7 +273,7 @@
               <p class="single_cat_left_content_meta">by <span>John Doe</span> |  29 comments</p>
             </div>
             <div class="single_cat_left_content floatleft">
-              <h3>Lorem ipsum dolor sit amet conse ctetur adipiscing elit </h3>
+              <h3>Lorem  dolor sit amet conse ctetur adipiscing elit </h3>
               <p>Nulla quis lorem neque, mattis venenatis lectus. In interdum ullamcorper dolor ...interdum</p>
               <p class="single_cat_left_content_meta">by <span>John Doe</span> |  29 comments</p>
             </div>
